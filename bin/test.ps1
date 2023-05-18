2ce3fb5dbbea17fe1ae55284bc4166056baded2c
Amin Jamali <ajamali@vmware.com>
2023-05-18 15:22:36 +0000

    exit 1 when tests fail

--- bin/test.ps1
+++ bin/test.ps1
@@ -2,7 +2,6 @@ $ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
 ginkgo.exe -p -r --race -keep-going --randomize-suites --fail-on-pending
-$exitCode = $LastExitCode
-if ($exitCode -ne 0) {
-  Exit $exitCode
+if ($LastExitCode -ne 0) {
+  Exit 1
 }
